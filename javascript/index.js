908351d1cd49d715bd2977f95f1368cd0052e582
nmanyange <nmanyange30@gmail.com>
2024-09-17 03:26:53 +0200

    corrected some errors in the files.

--- javascript/index.js
+++ javascript/index.js
@@ -32,7 +32,7 @@ function updateCity(event) {
                     <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
                 </div>
             <div class="time">${cityTime.format("h:mm:ss")} 
-                <small>${cityTime.format(A)}</small>
+                <small>${cityTime.format("A")}</small>
             </div>
             </div>`
 }
